8056305b1b384ab6fbd5c3c96e5a46ca666f349b
danielsmith-wf <daniel.smith@workiva.com>
2014-09-09 11:52:11 -0500

    Updated array to correct type

--- src/js/shapes.js
+++ src/js/shapes.js
@@ -2,14 +2,22 @@ var shapes = {}
 
 shapes.n_gon = function(sides) {
     var vertices = new Float32Array([0, 0]);
-    var indices = [];
+    var indices = new Uint16Array([]);
 
     var angle = Math.PI / sides;
 
     for (var i = 0; i < sides; i++)
     {
         vertices.push(Math.cos(i*angle), Math.sin(i*angle));
-        indices.push(0, i, i+1);
+        
+        if (i < sides - 1)
+        {
+            indices.push(0, i, i+1);
+        }
+        else
+        {
+            indices.push(0, i, 1);
+        }
     }
     
     return {vertices: vertices, indices: indices};

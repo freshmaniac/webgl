42c12bcc1fae327b85f7226ccbfdb27b4c1bed02
freshmaniac <smittydjames@gmail.com>
2014-09-09 22:40:56 -0500

    Finished HW1 problem 1

--- src/js/shapes.js
+++ src/js/shapes.js
@@ -1,21 +1,21 @@
 var shapes = {};
 
 shapes.n_gon = function (sides) {
-	var vertices = new Float32Array([0, 0]);
-	var indices = new Uint16Array([]);
+	var vertices = [0, 0];
+	var indices = [];
 
-	var angle = Math.PI / sides;
+	var angle = 2 * Math.PI / sides;
 
 	for (var i = 0; i < sides; i++) {
 		vertices.push(Math.cos(i * angle), Math.sin(i * angle));
 
 		if (i < sides - 1) {
-			indices.push(0, i, i + 1);
+			indices.push(0, i + 1, i + 2);
 		}
 		else {
-			indices.push(0, i, 1);
+			indices.push(0, i + 1, 1);
 		}
 	}
 
-	return {vertices: vertices, indices: indices};
+	return {vertices: new Float32Array(vertices), indices: new Uint16Array(indices)};
 };
